0d1b2ea15b921c5101d5454d1086ca63ca639ae9
Ruslan <ruslan.shtybaev@gmail.com>
2022-02-22 20:19:55 +0300

    add worked logic

--- AutorizationWindowOnUIKit/ViewController.swift
+++ AutorizationWindowOnUIKit/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var loginTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    let login = "ALEX"
+    let password = "Password"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
 
 
+    
+    @IBAction func enterButtornPressed() {
+        guard login == loginTextField.text && password == passwordTextField.text else {return showAlert(title: "Wrong login or password!", message: "Please will try agane.")}
+    }
+    
+    
+    
 }
 
+extension ViewController {
+    private func showAlert(
+        title: String,
+        message: String,
+        textField: UITextField? = nil
+    ) {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+    )
+        let okAction = UIAlertAction(
+            title: "OK",
+            style: .default
+        )
+        
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
