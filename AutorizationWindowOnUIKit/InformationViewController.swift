d0ca999c05acfb7cfd323ad46472a6d0390afc6d
Ruslan <ruslan.shtybaev@gmail.com>
2022-02-22 19:54:55 +0300

    add second ViewController

---
+++ AutorizationWindowOnUIKit/InformationViewController.swift
@@ -0,0 +1,20 @@
+//
+//  InformationViewController.swift
+//  AutorizationWindowOnUIKit
+//
+//  Created by Руслан Штыбаев on 22.02.2022.
+//
+
+import UIKit
+
+class InformationViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func CloseButtonPresed() {
+        dismiss(animated: true)
+    }
+}
